437a4b6afbae09d022f0c65ae51fd4c98d560b6b
xinyang-qiu <31750896+xinyang-qiu@users.noreply.github.com>
2018-04-08 23:26:37 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import matplotlib.malb as mlab
+import matplotlib.pyplot as plt
 
 def count_null(df):
 	c_names = df.columns
@@ -17,7 +19,22 @@ def count_uniqueness(df):
 		list_uniqueness.append(df.select(c).distinct().count())
 	for i in range(len(c_names)):
 		print(c_names[i], list_uniqueness[i])
+		
+def plot_hist(df):
+        c_names = df.columns
+        height = np.array(df[1])
+        bins = np.array(df[0])
+        mid_point = bins[:-1]
+        widths =[abs(i-j) for i, j in zip(bins[:-1],bins[1:])]
+        bar = plt.bar(mid_point,height,width = widths)
+        return bar
 
+def show_hist(df):
+        c_names = df.columns
+        for c in c_names:
+                col_hist = df.select(c).flatMap(kambda x:x).histogram(10)
+                plot_hist(col_hist)
+		
 if __name__ == "__main__":
 	spark = SparkSession \
 		.builder \
