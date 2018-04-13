b09ed8a30a8e22ae6545e4a2cd5da6937a9268bf
xinyang-qiu <31750896+xinyang-qiu@users.noreply.github.com>
2018-04-13 09:35:15 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import numpy as np
+from pyspark import SparkContext
 
 def count_null(df):
 	c_names = df.columns
@@ -18,25 +20,40 @@ def count_uniqueness(df):
 	for i in range(len(c_names)):
 		print(c_names[i], list_uniqueness[i])
 		
-def hist_list(df):
+def hist_list(df,ca_pr_type):
         c_names = df.columns
         l = []
         for c in c_names:
                 dis = df.select(c).distinct().count()
                 count = df.select(c).count()
                 print(c)
-                if dis < 0.6 * count:
+                if dis < 0.9 * count:
                         try:
-                                bins = int(0.01*dis)
-                                #print("Numerical")
-                                temp = df.select(c).rdd.flatMap(lambda x: x).histogram(bins)
+                                bins = int(dis*2/np.log10(dis))
+                                ran,num = df.select(c).rdd.flatMap(lambda x: x).histogram(bins)
                                 print("Numerical")
-                                print(temp)  
+                                print("Range:{}".format(np.round(ran,2)))
+                                print("Count:{}".format(np.round(num,1)))
                         except TypeError:
+
                                 print("Categorical")
                                 temp = df.select(c).rdd.flatMap(lambda x: x) \
                                         .map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)
-                                print(temp.collect())
+                                if ca_pr_type == "all":
+                                        output = temp
+                                else:
+                                        #car_pr_type like "top_5","bottom_10",etc.
+                                        print(ca_pr_type)
+                                        order,num = ca_pr_type.split("_",1)
+                                        num = int(num)
+                                        if order == "top":
+                                                output = sc.parallelize(temp.takeOrdered(num,lambda x: -x[1]))
+                                        else:
+                                                output = sc.parallelize(temp.takeOrdered(num,lambda x: x[1]))
+                                output = output.map(lambda x:"Name:%s  Count:%s" %(x[0],x[1]))
+                                print(output.collect())
+                else:
+                        print("NOT Included")
 		
 if __name__ == "__main__":
 	spark = SparkSession \
@@ -46,5 +63,5 @@ if __name__ == "__main__":
 	df = spark.read.json(sys.argv[1], multiLine = True)
 	count_null(df)
 	count_uniqueness(df)
-	hist_list(df)
+	hist_list(df,"top_5")
 
