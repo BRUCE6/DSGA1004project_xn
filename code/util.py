f0f862b3e8f885be073180325bc28ef8739a7126
xinyang-qiu <31750896+xinyang-qiu@users.noreply.github.com>
2018-04-25 13:48:02 -0400

    Add files via upload

--- code/util.py
+++ code/util.py
@@ -1,11 +1,15 @@
 import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+#import pandas as pd
 import numpy as np
 from pyspark import SparkContext
+from pyspark.mllib.clustering import KMeans, KMeansModel
 import itertools
-from datasketch import MinHash
-
+#from datasketch import MinHash
+#import matplotlib.mlab as mlab
+#import matplotlib.pyplot as plt
+sc = SparkContext()
 def count_null(df):
 	c_names = df.columns
 	list_null = []
@@ -14,7 +18,7 @@ def count_null(df):
 	#for i in range(len(c_names)):
 	#	print(c_names[i], list_null[i])
 	return list_null
-		
+	
 def count_distinct(df):
 	c_names = df.columns
 	list_distinct = []
@@ -24,105 +28,129 @@ def count_distinct(df):
 	#	print(c_names[i], list_distinct[i])
 	return list_distinct
 
-sc = SparkContext()		
 def hist_list(df,ca_pr_type):
-    c_names = df.columns
-        l = []
-        for c in c_names:
-            dis = df.select(c).distinct().count()
-            count = df.select(c).count()
-            print("Column Name: {}".format(c))
-            if dis < 0.9 * count:
-                try:
-                    bins = int(dis*2/np.log10(dis))
-                    ran,num = df.select(c).rdd.flatMap(lambda x: x).histogram(bins)
-                    print("Type: Numerical")
-                    a = []
-                        for i in range(1,len(ran)):
-                            a.append("Range:(%s,%s) Count: %s" % (np.round(ran[i-1],2),np.round(ran[i],2),np.round(num[i-1],1)))
-                                print(a)
-                    except TypeError:
-                        temp = df.select(c).rdd.flatMap(lambda x: x) \
-                            .map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)
-                                t = temp
-                                ls_p = t.map(lambda x:x[0]).take(10)
-                                split = []
-                                pattern = []
-                                for i in range(10):
-                                    t1 = list(str(ls_p[i]))
-                                    split.append(t1)
-                                new_df = spark.createDataFrame(split)
-                                c_new_names = new_df.columns
-                                for c_new in c_new_names:
-                                    temp1 = new_df.select(c_new).rdd.flatMap(lambda x: x).collect()
-                                    pattern.append(generate_pattern(temp1))
-                                
-                                if sum([i == 'd' for i in pattern[0:4]]) == 4 and sum([i == 'd' for i in pattern[5:7]]) == 2:
-                                    print ("Type: Date")
-                                else:
-                                    print("Type: Categorical")
+	c_names = df.columns
+	l = []
+	num_col = []
+	for c in c_names:
+		dis = df.select(c).distinct().count()
+		count = df.select(c).count()
+		print("Column Name: {}".format(c))
+		#drop unique columns
+		if dis < 0.9 * count:
+			try:
+				bins = int(dis/np.log10(dis))
+				ran,num = df.select(c).rdd.flatMap(lambda x: x).histogram(bins)
+				print("Type: Numerical")
+				a = []
+				for i in range(1,len(ran)):
+					a.append("Range:(%s,%s) Count: %s" % (np.round(ran[i-1],2),np.round(ran[i],2),np.round(num[i-1],1)))
+				print(a)
+				num_col.append(c)
+			except TypeError:
+				temp = df.select(c).rdd.flatMap(lambda x: x) \
+					.map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)
+				t = temp
+				ls_p = t.map(lambda x:x[0]).take(10)
+				split = []
+				pattern = []
+				for i in range(10):
+					t1 = list(str(ls_p[i]))
+					split.append(t1)
+				new_df = spark.createDataFrame(split)
+				c_new_names = new_df.columns
+				for c_new in c_new_names:
+					temp1 = new_df.select(c_new).rdd.flatMap(lambda x: x).collect()
+					pattern.append(generate_pattern(temp1))
+				
+				if sum([i == 'd' for i in pattern[0:4]]) == 4 and sum([i == 'd' for i in pattern[5:7]]) == 2:
+					print ("Type: Date")	
+				else:
+					print("Type: Categorical")	
 
-                                if ca_pr_type == "all":
-                                    output = temp
-                                else:
-                                    #car_pr_type like "top_5","bottom_10",etc.
-                                    print(ca_pr_type)
-                                    order,num = ca_pr_type.split("_",1)
-                                    num = int(num)
-                                    if order == "top":
-                                        output = sc.parallelize(temp.takeOrdered(num,lambda x: -x[1]))
-                                    else:
-                                        output = sc.parallelize(temp.takeOrdered(num,lambda x: x[1]))
-                                output = output.map(lambda x:"Name:%s  Count:%s" %(x[0],x[1]))
-                                print(output.collect())
-            else:
-                print("Distribution / Groups NOT Included")
-
-			
+				if ca_pr_type == "all":
+					output = temp
+				else:
+					#car_pr_type like "top_5","bottom_10",etc.
+					print(ca_pr_type)
+					order,num = ca_pr_type.split("_",1)
+					num = int(num)
+					if order == "top":
+						output = sc.parallelize(temp.takeOrdered(num,lambda x: -x[1]))
+					else:
+						output = sc.parallelize(temp.takeOrdered(num,lambda x: x[1]))
+				output = output.map(lambda x:"Name:%s  Count:%s" %(x[0],x[1]))
+				print(output.collect())
+		else:
+			print("Distribution / Groups NOT Included")
+	return(num_col)	
+def check_d(ls):
+	if sum([ls[i].isdigit() for i in range(len(ls))]) != len(ls): 
+		return False
+	else:
+		return True
+def check_c(ls):
+	check_char =True
+	for i in range(1,len(ls)):
+		check_char = check_char and ls[i] == ls[i-1]
+	return [check_char,ls[0]]
+def check_l(ls):
+	check_letter = True
+	for i in range(len(ls)):
+		check_letter = check_letter and ls[i].isalpha()
+	return check_letter
+def check_l_u(ls):
+	check_letter_U = True
+	for i in range(len(ls)):
+		check_letter_U = check_letter_U and ls[i].isupper()
+	return check_letter_U
+def check_l_l(ls):
+	check_letter_l = True
+	for i in range(len(ls)):
+		check_letter_l = check_letter_l and ls[i].islower()
+	return check_letter_l
 def generate_pattern(ls):
-        if sum([ls[i].isdigit() for i in range(len(ls))]) == len(ls):
-                return 'd'
-        elif sum([ls[i].isalpha() for i in range(len(ls))]) == len(ls):
-                if sum([ls[i].isupper() for i in range(len(ls))]) == len(ls):
-                        return 'A'
-                elif sum([ls[i].islower() for i in range(len(ls))]) == len(ls):
-                        return 'a'
-                else:
-                        return 'l'
-        elif ls.count(ls[0]) == len(ls):
-                return ls[0]
-        else:
-                return "_ "
+	if sum([ls[i].isdigit() for i in range(len(ls))]) == len(ls):
+		return 'd'
+	elif sum([ls[i].isalpha() for i in range(len(ls))]) == len(ls):
+		if sum([ls[i].isupper() for i in range(len(ls))]) == len(ls):
+			return 'A'
+		elif sum([ls[i].islower() for i in range(len(ls))]) == len(ls):
+			return 'a'
+		else:
+			return 'l'
+	elif ls.count(ls[0]) == len(ls):
+		return ls[0]
+	else: 
+		return "_ "
 
 def print_pattern(df):
-        c_names = df.columns
-        for c in c_names:
-                index = True
-                count = df.select(c).count()
-                if count > 100:
-                        count = int(np.log2(count)) * 10
-                get_val = df.select(c).rdd.flatMap(lambda x: x).takeSample(False,count)
-                split = []
-                pattern = []
-                for i in range(count):
-                        temp = list(str(get_val[i]))
-                        if len(list(str(get_val[0]))) != len(temp):
-                                index = False
-                        else:
-                                split.append(temp)
-                if index:
+	c_names = df.columns
+	for c in c_names:
+		index = True
+		count = df.select(c).count()
+		if count > 100:
+			count = int(np.log2(count))*10
+		get_val = df.select(c).rdd.flatMap(lambda x: x).takeSample(False,count)
+		split = []
+		pattern = []
+		for i in range(count):
+			temp = list(str(get_val[i]))
+			if len(list(str(get_val[0]))) != len(temp):
+				index = False
+			else:
+				split.append(temp)
+		if index:
 			new_df = spark.createDataFrame(split)
-                        c_new_names = new_df.columns
-                        for c_new in c_new_names:
-                                temp = new_df.select(c_new).rdd.flatMap(lambda x: x).collect()
-                                pattern.append(generate_pattern(temp))
-                        if pattern.count("_ ") > 5:
-                                print("{0}: {1} (number of _ :{2})".format(c,''.join(pattern),pattern.count("_ ")))
-                        else:
-                                print("{0}: {1}".format(c,''.join(pattern)))
+			c_new_names = new_df.columns
+			for c_new in c_new_names:
+				temp = new_df.select(c_new).rdd.flatMap(lambda x: x).collect()
+				pattern.append(generate_pattern(temp))
+			if pattern.count("_ ") > 5:
+				print("{0}: {1} (number of _ :{2})".format(c,''.join(pattern),pattern.count("_ ")))
+			else:
+				print("{0}: {1}".format(c,''.join(pattern)))
 
-				
-		
 def minhash(df):
 	c_names, c_pairs = [], []
 	for name, dtype in df.dtypes:
@@ -143,6 +171,7 @@ def minhash(df):
 		s2 = set(data2)
 		actual_jaccard = float(len(s1.intersection(s2)))/float(len(s1.union(s2)))
 		print("Actual Jaccard for data1 and data2 is", actual_jaccard)
+
 
 def candidate_key(df):
 	num = df.count()
@@ -187,6 +216,12 @@ def candidate_key(df):
 				list_candidate.append(key)
 	#print(list_candidate)
 	return list_candidate
+###cluster to generate patterns
+def error(point):
+	center = clusters.centers[clusters.predict(point)]
+	return sqrt(sum([x**2 for x in (point - center)]))
+
+#def cluster_pattern(ls):
 	
 if __name__ == "__main__":
 	spark = SparkSession \
@@ -194,11 +229,10 @@ if __name__ == "__main__":
 		.appName("compute null value numbers") \
 		.getOrCreate()
 	df = spark.read.json(sys.argv[1], multiLine = True)
-	#print(num)
-	#list_null = count_null(df)
-	#list_distinct = count_distinct(df)
-	#print(list_distinct)
-	#hist_list(df,"top_10")
+	print(count_null(df))
+	print(count_distinct(df))
 	print_pattern(df.na.drop(how="all"))
-	print(candidate_key(df))
-
+	#minhash(df)
+	#candidate_key(df)
+	a = hist_list(df,"top_5")
+	print(a)
