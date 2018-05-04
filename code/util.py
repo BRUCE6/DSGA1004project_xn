4ea1c5e79f4e4bd65d12a28dbbd04a9bef673000
Xiaoran Ni <xn287@nyu.edu>
2018-05-04 04:37:12 -0400

    add foreign key relation

--- code/util.py
+++ code/util.py
@@ -124,8 +124,9 @@ def single_minhash(df, num):
 		#actual_jaccard = float(len(s1.intersection(s2)))/float(len(s1.union(s2)))
 		#print("Actual Jaccard for data1 and data2 is", actual_jaccard)
 
-def multi_minhash(df1, df2):
-	c_names1, c_names2 = []
+def multi_minhash(df1, num1, df2, num2):
+	c_names1 = []
+	c_names2 = []
 	for name, dtype in df1.dtypes:
 		if dtype == "string":
 			c_names1.append(name)
@@ -134,8 +135,12 @@ def multi_minhash(df1, df2):
 			c_names2.append(name)
 	for col1,col2 in itertools.product(c_names1, c_names2):
 		m1, m2 = MinHash(), MinHash()
-		data1 = df1.select(col1).rdd.flatMap(lambda x:x).collect()
-		data2 = df2.select(col2).rdd.flatMap(lambda x:x).collect()
+		#data1 = df1.select(col1).rdd.flatMap(lambda x:x).collect()
+		#data2 = df2.select(col2).rdd.flatMap(lambda x:x).collect()
+		count1 = int(np.sqrt(num1)) * 100
+		count2 = int(np.sqrt(num2)) * 100
+		data1 = df1.select(col1).rdd.flatMap(lambda x:x).takeSample(False, count1)
+		data2 = df2.select(col2).rdd.flatMap(lambda x:x).takeSample(False, count2)
 		for d in data1:
 			for i in ngrams(d,4):
 				m1.update(''.join(i).encode('utf-8'))
@@ -143,6 +148,19 @@ def multi_minhash(df1, df2):
 			for i in ngrams(d,4):
 				m2.update(''.join(i).encode('utf-8'))
 		print("MinHash Similarity for {} and {} is {}".format(col1, col2, m1.jaccard(m2)))
+
+# only foreign key referencing a column
+def foreign_key(df1, uni1, df2, uni2):
+	c_names1 = df1.columns
+	c_names2 = df2.columns
+	for col1, col2 in itertools.product(c_names1, c_names2):
+		data1 = df1.select(col1).rdd.flatMap(lambda x:x).collect()
+		data2 = df2.select(col2).rdd.flatMap(lambda x:x).collect()
+		len_inter = len(set(data1) & set(data2))
+		if len_inter == len(set(data1)) and uni2[c_names2.index(col2)]:
+			print("{} is a foreign key referencing {}".format(col1, col2))
+		elif len_inter == len(set(data2)) and uni1[c_names1.index(col1)]:
+			print("{} is a foreign key referencing {}".format(col2, col1))
 
 def set_nonunique(key_size, key, list_dict):
 	list_dict[key_size - 1][key] = "Non_unique"
@@ -310,11 +328,17 @@ if  __name__ == "__main__":
 		.appName("Data Profiler") \
 		.getOrCreate()
 	df = spark.read.json(sys.argv[1], multiLine = True)
+	df1 = spark.read.json(sys.argv[2], multiLine = True)
+	
 	print('*'*50)
 	print(sys.argv[1])
 	num = df.count()
 	print('{0} rows'.format(num))
-
+	print(sys.argv[2])
+	num1 = df1.count()
+	print('{0} rows'.format(num1))
+	
+	'''
 	print('*'*50)
 	print("Null value number:")
 	start = timeit.default_timer()
@@ -323,7 +347,8 @@ if  __name__ == "__main__":
 	for i in range(len(list_null)):
 		print("{0:15s}:{1}".format(df.columns[i], list_null[i]))
 	print('[{0:.2f}s]'.format(stop - start))
-
+	'''
+	
 	print('*'*50)
 	print("Column uniqueness:")
 	start = timeit.default_timer()
@@ -332,6 +357,17 @@ if  __name__ == "__main__":
 	stop = timeit.default_timer()
 	for i in range(len(list_uniqueness)):
 		print("{0:15s}:{1}".format(df.columns[i], list_uniqueness[i]))
+	print('[{0:.2f}s]'.format(stop - start))
+	
+
+	print('*'*50)
+	print("Column uniqueness:")
+	start = timeit.default_timer()
+	list_distinct1 = count_distinct(df1)
+	list_uniqueness1 = [n == num1 for n in list_distinct1]
+	stop = timeit.default_timer()
+	for i in range(len(list_uniqueness1)):
+		print("{0:15s}:{1}".format(df1.columns[i], list_uniqueness1[i]))
 	print('[{0:.2f}s]'.format(stop - start))
 	
 	'''
@@ -343,10 +379,13 @@ if  __name__ == "__main__":
 	for c in list_candidate:
 		print(c)
 	print('[{0:.2f}s]'.format(stop - start))
-	'''
+	
 
 	print('*'*50)
 	#print_pattern(df.na.drop(how="all"))
 	single_minhash(df, num)
-	#multi_minhash(df1,df2)
+	'''
+	print('*'*50)
+	#multi_minhash(df, num, df1, num1)
 	#print_hist(df,"top_10")
+	foreign_key(df, list_uniqueness, df1, list_uniqueness1)
